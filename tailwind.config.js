7b1014f8fe17ac8edc119d09bc86d238ec6fe989
Puneet Dhiman <puneetdhiman56@gmail.com>
2023-09-17 17:11:49 +0530

    new resume

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./index.html"],
+	theme: {
+		fontSize: {
+			base: "0.8rem",
+			xl: "1rem",
+			"2xl": "1.25rem",
+			"3xl": "1.563rem",
+			"4xl": "1.953rem",
+			"5xl": "2.441rem",
+			"6xl": "3.052rem",
+		},
+		extend: {},
+	},
+	plugins: [],
+};
